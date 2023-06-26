dc162cf1346664f5752a15d6a27ddd1fe45d595a
retpy <shad02105@gmail.com>
2023-06-26 20:06:55 +0600

    registered all models in admin

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+from django.contrib.admin import register, ModelAdmin
 
-# Register your models here.
+from apps.posts.models import Post, Comment, Like
+
+
+@register(Post)
+class PostAdmin(ModelAdmin):
+    list_display = ('id', 'title', 'user', 'created_at')
+    list_display_links = ('id', 'title')
+    list_filter = ('user', 'created_at',)
+
+
+@register(Comment)
+class CommentAdmin(ModelAdmin):
+    list_display = ('id', 'post', 'user', 'created_at')
+    list_display_links = ('id', 'post')
+    list_filter = ('post', 'user', 'created_at',)
+
+
+@register(Like)
+class LikeAdmin(ModelAdmin):
+    list_display = ('id', 'user', 'created_at')
+    list_display_links = ('id', 'user')
+    list_filter = ('user', 'created_at',)
